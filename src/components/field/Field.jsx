cb70d636dc596f4f2f1bf1ec01329e1bddd7a928
NamanSnag <nnag44141@gmail.com>
2023-03-31 19:17:12 +0530

    Style and handler actiont

--- src/components/field/Field.jsx
+++ src/components/field/Field.jsx
@@ -1,30 +1,94 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
 import "./style.scss";
 
-const Field = () => {
+const Field = ({ field, index, schema, setSchema }) => {
+  const [input, setInput] = useState(false);
+
+  let fieldName = useRef();
+
+  // Delete field
+  const handleDeleteField = () => {
+    const newSchema = [...schema];
+    newSchema.splice(index, 1);
+    setSchema(newSchema);
+  };
+
+  // handle field change
+  const handleFieldChange = (e, field) => {
+    let newField = schema[index];
+
+    switch (field) {
+      case "name":
+        let name = fieldName.current.value;
+        newField = {
+          ...newField,
+          name: name,
+        }; 
+        schema[index] = newField;
+        setSchema([...schema])
+        setInput(false);
+        break;
+
+      case "type":
+        let type = e.target.value;
+
+        newField = {
+          ...newField,
+          type: type,
+        }; 
+        schema[index] = newField;
+        setSchema([...schema])
+        break;
+
+      case "value":
+        let value = e.target.checked;
+        newField = {
+          ...newField,
+          value: value,
+        }; 
+        schema[index] = newField;
+        setSchema([...schema])
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="field__container">
-      <p>1.</p>
+      <p>{index + 1}.</p>
+      <div className="field__detail">
+        <div className="field__type">
+          {input ? (
+            <input
+              type="text"
+              className="field__name"
+              ref={fieldName}
+              onDoubleClick={(e) => handleFieldChange(e, 'name')}
+            />
+          ) : (
+            <p onDoubleClick={() => setInput(true)}>{field.name}</p>
+          )}
 
-      <div className="field__type">
-        person
-        <select className="field__dropdown">
-          <option value="string">String</option>
-          <option value="number">Number</option>
-          <option value="boolean">Boolean</option>
-          <option value="object">Object</option>
-        </select>
-      </div>
+          <select className="field__dropdown" onChange={(e) => handleFieldChange(e, 'type')}>
+            <option value="string">String</option>
+            <option value="number">Number</option>
+            <option value="boolean">Boolean</option>
+            <option value="object">Object</option>
+          </select>
+        </div>
 
-      <div className="field__hover">
-        <label className="field__switch">
-          Required
-          <input type="checkbox" />
-          <span className="slider"></span>
-        </label>
-        <RiDeleteBin5Line className="delete" />
+        <div className="field__hover">
+          <label className="field__switch" >
+            Required
+            <input type="checkbox" onChange={(e) => handleFieldChange(e, 'value')}/>
+            <span className="slider"></span>
+          </label>
+          <RiDeleteBin5Line className="delete" onClick={handleDeleteField} />
+        </div>
       </div>
     </div>
   );
--- src/pages/userModel/UserModel.jsx
+++ src/pages/userModel/UserModel.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Field } from "../../components";
 import { BsPlus } from "react-icons/bs";
 
 import "./style.scss";
 
 const UserModel = () => {
+  const [schema, setSchema] = useState([]);
+
+  // add new field 
+  const handleAddField = () => {
+    setSchema([
+      ...schema,
+      { name: "Enter_Name", type: "string", value: false }
+    ])
+  }
+
   return (
     <div className="user__container">
       <div className="user__fields">
         <div className="user__head">
           <p>Field name and type</p>
-          <BsPlus className="user__plus" />
+          <BsPlus 
+          className="user__plus"
+          onClick={handleAddField}
+          />
         </div>
 
         <div className="user__field">
-          <Field />
-          <Field />
-          <Field />
-          <Field />
+          {
+            schema.length ? (
+              schema.map((field, index) => (
+              <Field 
+                key={index} 
+                field={field} 
+                index={index} 
+                setSchema={setSchema}
+                schema={schema}
+              />
+            ))
+            ) : (
+              <h3 style={{textAlign:'center', marginTop: '40px'}}>
+                Start creating User Schema By Clicking on add Button</h3>
+            )
+          }
         </div>
       </div>
     </div>
